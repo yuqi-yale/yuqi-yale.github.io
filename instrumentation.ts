afbd36e9de02f2c0bb042af88fe9856fa9058a62
yuqi-yale <yuqi.zhao@yale.edu>
2024-09-08 14:38:12 -0400

    install monitor

---
+++ instrumentation.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+import tracker from '@middleware.io/agent-apm-nextjs';
+
+export function register() {
+  tracker.track({
+      serviceName: "monitor",
+      accessToken: "mukekdlgqiccrwsqhlbihwmjrtxpjouaizzl",
+      target: "https://rmewh.middleware.io"
+  });
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,38 @@
 const config = {
-  swcMinify: true,
-  reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true,
-  },
-  transpilePackages: ["geist"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'yuqiweb.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
-      },
-    ],
-  },
-//   async redirects() {
-//     return [
-//       {
-//         source: '/notes/:slug*',
-//         destination: '/blogs/:slug*',
-//         permanent: true,
-//       },
-//     ];
-//   },
+    swcMinify: true,
+    reactStrictMode: true,
+    experimental: {
+        scrollRestoration: true,
+    },
+    transpilePackages: ["geist"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'yuqiweb.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+            },
+        ],
+    },
+    //   async redirects() {
+    //     return [
+    //       {
+    //         source: '/notes/:slug*',
+    //         destination: '/blogs/:slug*',
+    //         permanent: true,
+    //       },
+    //     ];
+    //   },
+    experimental: {
+        instrumentationHook: true
+    }
 };
 
 export default config;
