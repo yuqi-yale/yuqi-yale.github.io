2d268cfb451ce8272ae768fc12a339d1d381e566
yuqi-yale <yuqi.zhao@yale.edu>
2024-09-08 14:18:34 -0400

    new file:   server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const tracker = require('@middleware.io/node-apm'); 
+tracker.track({ 
+  serviceName: "monitor", 
+  accessToken: "mukekdlgqiccrwsqhlbihwmjrtxpjouaizzl", 
+  target: "https://rmewh.middleware.io",
+});
