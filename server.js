4186116004bd64fdf9d99bc6d01071ef80dce944
yuqi-yale <yuqi.zhao@yale.edu>
2024-09-08 14:29:11 -0400

    Rollback

--- server.js
+++
@@ -1,6 +0,0 @@
-const tracker = require('@middleware.io/node-apm'); 
-tracker.track({ 
-  serviceName: "monitor", 
-  accessToken: "mukekdlgqiccrwsqhlbihwmjrtxpjouaizzl", 
-  target: "https://rmewh.middleware.io",
-});
